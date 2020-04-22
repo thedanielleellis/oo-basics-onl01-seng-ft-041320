08345c034567436260a331d39aba82d40d6101c6
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-22 17:27:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,12 @@
 class Book
- 
-  def initialize(title)
-  end
- 
-end
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
